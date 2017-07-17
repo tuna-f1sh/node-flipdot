781f388409b4d5e6725dc466ca93a9fdec41cf8e
John Whittington <john@jbrengineering.co.uk>
2017-07-17 17:17:29 +0100

    Dev on a plane - not checked with hardware

--- examples/test.js
+++ examples/test.js
@@ -5,16 +5,15 @@ var flippy = new FlipDot('/dev/tty.wchusbserial1420',5,7,56);
 var matrix = new Array(56).fill(0);
 
 matrix[1] = 0xFF;
-var x = flippy.encode(matrix);
-
-flippy.packet.data = x;
+data = flippy.encode(matrix);
+var x = flippy.writeText('John');
 
 flippy.on("error", function(err) {
   console.log(err);
 });
 
 flippy.on("open", function() {
-  flippy.send(flippy.packet, function() {
+  flippy.send(x, function() {
     console.log("Sent");
   });
   flippy.close();
--- flipdot.js
+++ flipdot.js
@@ -22,6 +22,9 @@ var Emitter = require("events").EventEmitter;
 // External Dependancies
 var SerialPort = require('serialport');
 
+// Modules
+var figlet = require('figlet');
+
 function FlipDot(port, addr, rows, columns, callback) {
   if (typeof port === "function" || typeof port === "undefined") {
     callback = port;
@@ -50,13 +53,19 @@ function FlipDot(port, addr, rows, columns, callback) {
   this.columns = columns || 56;
   this.data = ((rows * columns) / 8); // frame data size is div 8 because dots sent as bytes (actual data sent will be twice this due to ascii encoding of hex byte...
   this.col_bytes = rows / 8; // number of bytes in each column
-  this.res = this.byteToAscii(this.data & 0xFF);
+  this.ldata = (this.columns * this.col_bytes * 2);
+  this.res = this.byteToAscii(this.data & 0xFF); // resolution in Hanover format for header
+  // packet on display
   this.packet = {
     header : [this.hchar, 0x31, 0x35, this.res[0], this.res[1]],
     data : [],
     footer : [this.fchar, 0x00, 0x00],
   };
-  this.buffer = Buffer.alloc(5 + (this.columns * this.col_bytes * 2) + 3);
+
+  // data stream sent to display
+  this._buffer = Buffer.alloc(this.packet.header.length + this.ldata + this.packet.footer.length);
+  this._queue = [];
+  this._busy = false;
 
   // console.log(this.packet);
 
@@ -76,6 +85,27 @@ function FlipDot(port, addr, rows, columns, callback) {
     this.emit("open");
   }.bind(this));
 
+  this.serial.on("close", function() {
+    this.emit("close");
+    console.log("close");
+  }.bind(this));
+
+  this.serial.on("disconnect", function() {
+    this._busy = false;
+    this._queue = [];
+    this.emit("disconnect");
+    console.log("disconnect");
+  }.bind(this));
+
+  this.serial.on("error", function(error) {
+    if (typeof callback === "function") {
+      callback(error);
+    } else {
+      if (this.debug) console.log('Error: ' + error);
+      this.emit("error", error);
+    }
+  }.bind(this));
+
 }
 
 FlipDot.prototype = Object.create(Emitter.prototype, {
@@ -85,16 +115,73 @@ FlipDot.prototype = Object.create(Emitter.prototype, {
 });
 
 FlipDot.prototype.write = function(data) {
-  if (this.debug) console.log("Writing serial data, size: " + data.length, " : " + data.toString('hex'));
+  if (this.debug) console.log("Writing serial data, size: " + data.length, "B : " + data.toString('hex'));
   this.serial.write(data);
 };
 
 FlipDot.prototype.writeDrain = function(data, callback) {
-  if (this.debug) console.log("Writing serial data, size: " + data.length, "b : " + data.toString('hex'));
+  if (this.debug) console.log("Writing serial data, size: " + data.length, "B : " + data.toString('hex'));
   this.serial.write(data, 'ascii', function () {
     this.serial.drain(callback);
   }.bind(this));
 };
+
+FlipDot.prototype.matrix = function(row = this.rows, col = this.columns) {
+  var x = new Array(row).fill(0);
+  for (var i = 0; i < row; i++) {
+    x[i] = new Array(col).fill(0);
+  }
+
+  return x;
+}
+
+FlipDot.prototype.matrixToBytes = function(matrix) {
+  var x = new Array(matrix[0].length * this.col_bytes).fill(0);
+
+  // TODO: Scale to include flipdots with more than 1 byte per column
+  for (j = 0; j < x.length; j++) {
+    for (i = 0; i < 8; i++) {
+      x[j] += (matrix[i][j] & 0x01) << i;
+    }
+  }
+
+  return x;
+}
+
+FlipDot.prototype.writeText = function(text, font = 'Doom') {
+  var aart = figlet.textSync(text, {
+    font: font,
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  });
+
+  console.log(aart);
+
+  // convert string to array at line breaks
+  aart = aart.split('\n');
+
+  // get a matrix to fill TODO: length of string and que excess data
+  mat = this.matrix();
+  console.log(mat);
+  
+  // fill matrix with on/off char/void
+  aart.forEach(function(row,i) {
+    for (j = 0; j < row.length; j++) {
+      mat[i][j] = ( (row.charAt(j) === '') || (row.charAt(j) === ' ') ) ? 0 : 1;
+    }
+  });
+
+  // convert matrix to column hex array
+  data = this.matrixToBytes(mat);
+
+  // encode it for Hanover display
+  data = this.encode(data);
+
+  // set data to property for next send
+  this.data = data;
+
+  return data;
+}
 
 FlipDot.prototype.byteToAscii = function(byte) {
   var asciichars = new Buffer(2);
@@ -120,13 +207,18 @@ FlipDot.prototype.byteToAscii = function(byte) {
 
 FlipDot.prototype.encode = function(matrix) {
   var data = [];
-  var flipdot = this;
+  var bytes = [];
 
-  matrix.forEach(function(byte, i) {
+  // run through matrix, replacing chars with 0 or 1 for on/off
+  // TODO: look up string replace in node
+
+  // TODO: Use binstring to parse string based matrix into hex rows/bytes and create byte array
+
+  matrix.forEach(function(byte) {
     data = data.concat(this.byteToAscii(byte));
   }.bind(this));
 
-  // if (this.debug) console.log(data);
+  if (this.debug) console.log("Encoded Data: " + data);
 
   return data;
 };
@@ -158,27 +250,44 @@ FlipDot.prototype.__checksum__ = function(packet) {
   return crc;
 };
 
-FlipDot.prototype.send = function(packet, callback) {
-  var crc = this.__checksum__(packet);
+FlipDot.prototype.send = function(data, callback) {
+
+  // check data length and que if longer than display/pad if less
+  if ( data.length > this.ldata ) {
+    this.packet.data = data.slice(0,this.ldata)
+    for (i = this.ldata; i <= data.length; i += this.ldata) {
+      this._queue.push(data.slice(i,i+this.ldata));
+    }
+  } else if ( data.length < this.ldata) {
+    // this.data = data.push
+  }
+  
+  // stick data in the packet if passed otherwise use current data
+  if (typeof this._que !== "undefined") {
+    this.packet.data = this_que.pop();
+  } else {
+    if (typeof data !== "undefined") this.packet.data = data;
+  }
+
+  // calculate CRC
+  var crc = this.__checksum__(this.packet);
   var footer = this.byteToAscii(crc);
 
-  packet.footer[1] = footer[0];
-  packet.footer[2] = footer[1];
+  // write footer with CRC to packet
+  this.packet.footer[1] = footer[0];
+  this.packet.footer[2] = footer[1];
 
-  head = Buffer.from(packet.header);
-  msg = Buffer.from(packet.data);
-  footer = Buffer.from(packet.footer);
+  // make into binary buffers for serialport
+  head = Buffer.from(this.packet.header);
+  msg = Buffer.from(this.packet.data);
+  footer = Buffer.from(this.packet.footer);
 
-  this.buffer = Buffer.concat([head, msg, footer]);
+  // update object and con concat the packet into one stream
+  // this.packet = packet;
+  this._buffer = Buffer.concat([head, msg, footer]);
 
-  console.log(this.buffer);
-  console.log(packet.header);
-  console.log(packet.footer);
-  console.log(this.buffer.length);
-
-  this.packet = packet;
-
-  this.writeDrain(this.buffer, callback);
+  // write via serial
+  this.writeDrain(this._buffer, callback);
 };
 
 /**
