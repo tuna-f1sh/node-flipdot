97b9466f24164695b8a7795353dfdb80717e2336
John Whittington <john@jbrengineering.co.uk>
2017-07-17 13:27:09 +0100

    Adding serial test script

---
+++ serial.js
@@ -0,0 +1,16 @@
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/tty.wchusbserial1420', {
+  baudRate: 4800
+});
+
+port.write(new Buffer(256), function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message);
+  }
+  console.log('message written');
+});
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+});
