c654ab19927ac73b1fc55dacb98922da142ade6a
John Whittington <john@jbrengineering.co.uk>
2017-07-18 06:33:26 +0000

    Test script with text array sending

--- examples/test.js
+++ examples/test.js
@@ -1,20 +1,32 @@
 var FlipDot = require('../flipdot.js');
 
-var flippy = new FlipDot('/dev/tty.wchusbserial1420',5,7,56);
+var flippy = new FlipDot('/dev/ttyUSB0',5,7,56);
 
-var matrix = new Array(56).fill(0);
+var matrix = new Array(56).fill(0xff);
 
 matrix[1] = 0xFF;
 data = flippy.encode(matrix);
-var x = flippy.writeText('John');
+flippy.send(data);
+//var x = flippy.writeText('Hello World!', '3x5');
+
+var i = 0;
+
+frames = [
+  "Hello",
+  "World!!",
+];
 
 flippy.on("error", function(err) {
   console.log(err);
 });
 
 flippy.on("open", function() {
-  flippy.send(x, function() {
-    console.log("Sent");
-  });
-  flippy.close();
+  cycle = setInterval( function() {
+    text = flippy.writeText(frames[i]);
+    flippy.send(text, function() {
+      console.log("Sent");
+      (i < (frames.length - 1)) ? i++ : clearInterval(cycle);
+    });
+  }, 1000);
+  //flippy.close();
 });
